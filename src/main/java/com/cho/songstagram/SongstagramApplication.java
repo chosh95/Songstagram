e9731c5d570b763fc3f6c4070e0ebc8c0b7f21b5
chosh95 <chosh-95@hanmail.net>
2020-07-06 14:29:51 +0900

    first commit

---
+++ src/main/java/com/cho/songstagram/SongstagramApplication.java
@@ -0,0 +1,13 @@
+package com.cho.songstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongstagramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SongstagramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cho/songstagram/SongstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cho.songstagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongstagramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
